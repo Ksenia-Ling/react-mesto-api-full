83c7066c3e6f844eab0a320c59e7e023efa91359
Ksenia Kozyreva <oestara91@gmail.com>
2022-09-30 00:32:06 +0300

    исправлено рег. выражение для проверки ссылок

--- backend/utils/regex.js
+++ backend/utils/regex.js
@@ -1,3 +1,3 @@
-const regExForUrl = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_.~#?&//=]*)/g;
+const regExForUrl = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g;
 
 module.exports = regExForUrl;
