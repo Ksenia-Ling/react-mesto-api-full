dab96c1342e66969cfb42941e8126b9c88f83d59
Ksenia Kozyreva <oestara91@gmail.com>
2022-09-29 23:55:46 +0300

    изменены параметры cookie

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -138,9 +138,8 @@ module.exports.login = (req, res, next) => {
 
             res.cookie('jwt', token, {
               maxAge: 3600000,
-              // sameSite: false,
+              sameSite: true,
               httpOnly: true,
-              // secure: true,
             });
             res.send({ data: user.toJSON() });
           } else {
